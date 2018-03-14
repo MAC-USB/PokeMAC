8925649e9baafc7f432b0b693c54a745c2cf6b45
Juan <jaoc1811@gmail.com>
2018-03-14 00:23:28 -0400

    Add scripts files pikachu.sh blastoise.sh charizard.sh snorlax.sh

---
+++ Script/scripts/charizard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat /home/meowth/drawings/charizard.txt 
+mplayer /home/meowth/sounds/charizard.mp3 >/dev/null 2>/dev/null
+
+mv /home/meowth/.poke_oculto/johto/monte_plateado/ /home/entrenador/johto/monte_plateado
+cd /home/entrenador/johto/monte_plateado
---
+++ Script/scripts/pikachu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat /home/meowth/drawings/pikachu.txt 
+mplayer /home/meowth/sounds/pikachu.mp3 >/dev/null 2>/dev/null
+
+if [ $(pwd) == '/home/entrenador/kanto/tunel_roca' ]
+then
+mv /home/meowth/.poke_oculto/kanto/tunel_roca/* /home/entrenador/kanto/tunel_roca/
+fi
