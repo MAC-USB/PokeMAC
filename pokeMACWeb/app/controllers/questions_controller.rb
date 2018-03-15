545d212b19cfd0b1b8b2511be12fe3b962a05aac
Manuel Faria <mfaria724@gmail.com>
2018-03-14 20:37:47 -0400

    Amin's Logic

--- pokeMACWeb/app/controllers/questions_controller.rb
+++ pokeMACWeb/app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
     end
 
     def index
+        redirect_to question_path(Question.find_by(title:params[:title])) if params[:title]
         @questions = Question.all
     end
 
@@ -10,11 +11,16 @@ class QuestionsController < ApplicationController
     def create
         @question = Question.new(question_params)
         @question.save
+
+        @user = User.find(current_user.id)
+        @user.questions[@question.id] = true
+        
         redirect_to @question
+
     end
 
     def show
-        @question = Question.where(params[:title])
+        @question = Question.find(params[:id])
     end
 
 
